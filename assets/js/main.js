ce0e7fb5b0644d57d1f0c2f9014b1cfded7e45ab
tony-reddington <tony.reddington@gmail.com>
2021-05-19 16:19:36 +0000

    Added function to add map to #map on index.html centering the map on Wicklow

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7.75,
+        center: {
+            lat: 53.00664,
+            lng: -6.71236
+        }
+    });
+}
