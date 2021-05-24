84a9c6fc800450255737ec38c11c9a99f4d8a3c2
tony-reddington <tony.reddington@gmail.com>
2021-05-24 20:29:01 +0000

    Targeted the form on contact.html to clear the inputs once the message is sent and all is ok

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,17 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "discoverwicklow", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "from_message": contactForm.message.value
-    }) 
+  emailjs.send("gmail", "discoverwicklow", {
+      "from_name": contactForm.name.value,
+      "from_email": contactForm.emailaddress.value,
+      "from_message": contactForm.message.value
+    })
     .then(
-        function(response) {
-            console.log("Message sent succesfully", response);
-        },
-        function(error) {
-            console.log("Message not sent", error);
-        }
+      function (response) {
+        console.log("Message sent succesfully", response);
+        document.getElementById("contactForm").reset();
+      },
+      function (error) {
+        console.log("Message not sent", error);
+      }
     );
-    return false;
+  return false;
 }
